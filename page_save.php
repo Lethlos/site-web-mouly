2f1eada93581b60dc9fc7d31da7826f110e0ff8f
Marin Bailhe <43707993+biadzok@users.noreply.github.com>
2023-10-25 11:57:55 +0200

    ajout début sauvegarde des données

---
+++ page_save.php
@@ -0,0 +1,27 @@
+<?php
+
+include 'xml_save.php';
+$rent = new SimpleXMLElement($xmlstr);
+
+if(isset($_POST["submit"])){ 
+    $rent->description = $_POST['user_message'];
+    
+    $rent->capacity->bed = $_POST['inputBed'];
+    $rent->capacity->place = $_POST['inputPlace'];
+    $rent->capacity->bedroom = $_POST['inputRoom'];
+    
+    $rent->price->week = $_POST['inputPriceWeek'];
+    $rent->price->night = $_POST['inputPriceNight'];
+    $rent->price->week_high = $_POST['inputPriceHighWeek'];
+    $rent->price->night_high = $_POST['inputPriceHighNight'];
+
+    $rent->location->address = $_POST['inputAdress'];
+    $rent->location->postcode = $_POST['inputCP'];
+    $rent->location->city = $_POST['inputCity'];
+
+    $rent->contact->phone = $_POST['inputNumTel'];
+    $rent->contact->mail = $_POST['inputMail'];
+    $rent->contact->facebook = $_POST['inputFacebook'];
+    $rent->contact->x = $_POST['inputX'];
+    $rent->contact->instagram = $_POST['inputInsta'];
+}
---
+++ xml_save.php
@@ -0,0 +1,32 @@
+
+<?php
+$xmlstr = <<<XML
+<?xml version='1.0' standalone='yes'?>
+<rent>
+    <description> enregistrez votre description ici</description>
+    <capacity>
+        <bed> 0</bed>
+        <place> 0</place>
+        <berdoom> 0</berdoom>
+    </capacity>
+    <price>
+        <week> 0</week>
+        <night> 0</night>
+        <week_high> 0</week_high>
+        <night_high> 0</night_high>
+    </price>
+    <location>
+        <address> 0</address>
+        <postcode> 0</postcode>
+        <city> 0</city>
+    </location>
+    <contact>
+        <phone> 0</phone>
+        <mail> 0</mail>
+        <facebook> 0</facebook>
+        <x> 0</x>
+        <instagram> 0</instagram>
+    </contact>
+</rent>
+XML;
+?>
