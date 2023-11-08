2c062f65e13a49a4726a2855f41caf461bd2aaf9
Marin Bailhe <43707993+biadzok@users.noreply.github.com>
2023-11-08 09:24:17 +0100

    sf

--- login.php
+++ login.php
@@ -1,8 +1,30 @@
 <?php
 
+require_once 'dbConfig.php';
+
 if (isset($_POST['submit'])) {
     $username = $_POST['username'];
-    $password = $_POST['password'];
+    $password = $_POST['password']; 
+
+    $result = $db->query("SELECT username, password FROM login");
+
+    if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()){
+            if ($row != null) {
+                $username_db = $row['username'];
+                $pwHash = $row['password'];
+                if ($username === $username_db) {
+                    if (password_verify($password, $pwHash)) {
+                        header('Location: edition_page.php');
+                        exit;
+                    }
+                    else {
+
+                    }
+                }
+            }
+        }
+    }
 
     // Vérifiez si le login et le mot de passe correspondent à "admin".
     if ($username === 'admin' && $password === 'admin') {
