fa408f60925a119cdc50e21ed0815f0113e37bf4
Marin Bailhe <43707993+biadzok@users.noreply.github.com>
2023-11-10 10:36:59 +0100

    zd

--- delete_calendar.php
+++ delete_calendar.php
@@ -5,10 +5,10 @@ require_once 'dbConfig.php';
 $start = (isset($_GET["start"])) ? new DateTime($_GET["start"]) : NULL;
 
 if ($start) {
-    $reservations = $db->query("SELECT start, end FROM calendar");
+    $reservations = $db->query("SELECT * FROM calendar");
     while ($row = $reservations->fetch_array()) {
         $id = $row["id"];
-        $startdb = $row["start"];
+        $startdb = new DateTime($row["start"]);
         if ($start == $startdb) {
             $delete = $db->query("DELETE from calendar where id = ".$id."");
         }
